97088448aa106fbd21d84324fd74973e85028c90
Romulo <romulotg12@gmail.com>
2023-09-27 05:58:08 -0300

    feat: sidebar added

---
+++ emaj-client/src/pages/Processos.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SideBar from "../ui/components/Sidebar";
+
+export default function Processos() {
+  return (
+    <div>
+    
+      <SideBar />
+    </div>
+  );
+};
