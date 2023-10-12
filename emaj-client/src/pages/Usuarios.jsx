8aad24e156f6b0a6830a32ea4411c656cecd24d0
Romulo <romulotg12@gmail.com>
2023-10-12 13:31:06 -0300

    feat: main pages added

---
+++ emaj-client/src/pages/Usuarios.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Usuarios() {
+  return (
+    <>
+    <div className="flex flex-col gap-10">
+      <div className="rounded-md border rounded-lg bg-white px-4 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+        <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
+          Usuários
+        </h4>
+
+      </div>
+    </div>
+    </>
+  );
+};
